c7f78f4276ae478d53bd927b466cbebc7d729441
SimonSchulz <zenkov241091@gmail.com>
2025-08-17 21:30:25 +0200

    fix validation

--- src/core/dto/base.query-params.input-dto.ts
+++ src/core/dto/base.query-params.input-dto.ts
@@ -1,18 +1,22 @@
 import { Type } from 'class-transformer';
 import { SortDirection } from '../enums/sort-direction.enum';
-import { IsEnum, IsInt, Min } from 'class-validator';
+import { IsEnum, IsInt, IsOptional, Min } from 'class-validator';
 
 export class BaseQueryParams {
+  @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
   pageNumber: number = 1;
+  @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(10)
   pageSize: number = 10;
+  @IsOptional()
   @IsEnum(SortDirection)
   sortDirection: SortDirection = SortDirection.Desc;
+
   calculateSkip() {
     return (this.pageNumber - 1) * this.pageSize;
   }
