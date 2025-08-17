7f4eab1c85ca7252a7781fdb89cee1c236609f68
SimonSchulz <zenkov241091@gmail.com>
2025-08-17 15:28:25 +0200

    add all exeption filter

--- src/core/exeptions/filters/all-exceptions.filter.ts
+++ src/core/exeptions/filters/all-exceptions.filter.ts
@@ -10,41 +10,32 @@ import { DomainExceptionCode } from '../domain-exception-codes';
 
 @Catch()
 export class AllHttpExceptionsFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost): void {
+  catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    const message = exception.message || 'Unknown exception occurred.';
-    const status = HttpStatus.INTERNAL_SERVER_ERROR;
-    const responseBody = this.buildResponseBody(request.url, message);
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Unknown exception occurred.';
 
-    response.status(status).json(responseBody);
-  }
-
-  private buildResponseBody(
-    requestUrl: string,
-    message: string,
-  ): ErrorResponseBody {
-    //TODO: Replace with getter from configService. will be in the following lessons
-    const isProduction = process.env.NODE_ENV === 'production';
-
-    if (isProduction) {
-      return {
-        timestamp: new Date().toISOString(),
-        path: null,
-        message: 'Some error occurred',
-        extensions: [],
-        code: DomainExceptionCode.InternalServerError,
-      };
+    // безопасная проверка, если exception - объект с нужными свойствами
+    if (typeof exception === 'object' && exception !== null) {
+      if ('status' in exception && typeof exception.status === 'number') {
+        status = exception.status;
+      }
+      if ('message' in exception && typeof exception.message === 'string') {
+        message = exception.message;
+      }
     }
 
-    return {
+    const responseBody: ErrorResponseBody = {
       timestamp: new Date().toISOString(),
-      path: requestUrl,
+      path: request.url,
       message,
       extensions: [],
       code: DomainExceptionCode.InternalServerError,
     };
+
+    response.status(status).json(responseBody);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appSetup } from './app.setup';
 import { DomainHttpExceptionsFilter } from './core/exeptions/filters/domain-exceptions.filter';
+import { AllHttpExceptionsFilter } from './core/exeptions/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   appSetup(app);
-  app.useGlobalFilters(new DomainHttpExceptionsFilter());
+  app.useGlobalFilters(
+    new DomainHttpExceptionsFilter(),
+    new AllHttpExceptionsFilter(),
+  );
   const PORT = process.env.PORT || 5001;
   await app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
